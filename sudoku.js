358026cb48fbfb7e2bca25fa406d6d3fbda7e4c2
Valery-Klepinin <Valenn1996@mail.ru>
2023-06-02 15:17:00 +0300

    fanc isSolver

--- sudoku.js
+++ sudoku.js
@@ -3,15 +3,39 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
-}
+function solve(boardString) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
+function isSolved(num, pos, board) {
+  const [r, c] = pos;
 
+  // проверка строк
+  for (let i = 0; i < size; i += 1) {
+    if (board[i][c] === num && i !== r) {
+      return false;
+    }
+  }
+  // проверка колонок
+  for (let i = 0; i < size; i += 1) {
+    if (board[r][i] === num && i !== c) {
+      return false;
+    }
+  }
+  // проверка сектора
+  const boxRow = Math.floor(r / boxSize) * boxSize;
+  const boxCol = Math.floor(c / boxSize) * boxSize;
+
+  for (let i = boxRow; i < boxRow + boxSize; i += 1) {
+    for (let j = boxCol; j < boxCol + boxSize; j += 1) {
+      if (board[i][j] === num && i !== r && j !== c) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 /**
@@ -19,9 +43,7 @@ function isSolved(board) {
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
